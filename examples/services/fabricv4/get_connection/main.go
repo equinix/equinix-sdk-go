a9aa5abdcccf41e200c89c71bc364c7cddf776a7
Tim Hogarty <thogarty@equinix.com>
2024-05-28 17:28:12 -0700

    Update example file to include command line input, remove terraform specific package, and add comments with details on package inclusion

--- examples/services/fabricv4/get_connection/main.go
+++ examples/services/fabricv4/get_connection/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/equinix/equinix-sdk-go/services/fabricv4"
 	"log"
-	"net/url"
 	"os"
 	"time"
 
 	"github.com/equinix/oauth2-go"
 	"github.com/hashicorp/go-retryablehttp"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
 )
 
 func main() {
+
+	// Pulls the connection_uuid from the command line so that you can input it with
+	// `go run main.go -connection_uuid="<your_connection_uuid>"`
+	var connection_uuid string
+	flag.StringVar(&connection_uuid, "connection_uuid", "", "")
+	flag.Parse()
+
+	// Initializes the Equinix oauth2 with the EQUINIX_API_CLIENTID and EQUINIX_API_CLIENTSECRET
+	// details set as environment variables.
 	ctx := context.Background()
 	clientId := os.Getenv("EQUINIX_API_CLIENTID")
 	clientSecret := os.Getenv("EQUINIX_API_CLIENTSECRET")
@@ -24,30 +32,36 @@ func main() {
 		BaseURL:      baseURL,
 	}
 	authClient := authConfig.New(ctx)
-	authClient.Transport = logging.NewTransport("Equinix", authClient.Transport)
 
-	transport := logging.NewTransport("Equinix Fabric (fabriv4)", authClient.Transport)
-
+	/*
+		The following code is setting up configuration of the
+		SDK for retries in the case that there is a network connection error or the server returns a 5XX error.
+		It is using Hashicorp's retryablehttp Go package but the same could be done with an alternative like req.
+		See https://req.cool/docs/tutorial/retry/ for details.
+		For this example we're doing the following:
+		* Adding auth to our retry client
+		* Setting the minimum wait time between retries to 1 second
+		* Setting the maximum time between retries to 60 seconds
+		* Returning our config using the StandardClient from retryablehttp that returns http.Client with our
+			config details
+	*/
 	retryClient := retryablehttp.NewClient()
-	retryClient.HTTPClient.Transport = transport
+	retryClient.HTTPClient.Transport = authClient.Transport
 	retryClient.RetryWaitMin = time.Second
 	retryClient.RetryWaitMax = time.Second * 60
 	standardClient := retryClient.StandardClient()
 
-	baseURLParsed, _ := url.Parse(baseURL)
-
+	// The following configuration allows us to add custom headers, add our retryable http.client to the SDK,
+	// and creates an Fabricv4 API client from the result
 	configuration := fabricv4.NewConfiguration()
-	configuration.Servers = fabricv4.ServerConfigurations{
-		fabricv4.ServerConfiguration{
-			URL: baseURLParsed.String(),
-		},
-	}
 	configuration.HTTPClient = standardClient
 	configuration.AddDefaultHeader("X-SOURCE", "API")
 	configuration.AddDefaultHeader("X-CORRELATION-ID", "asdfdkioinasdoinfiek183859573")
 	client := fabricv4.NewAPIClient(configuration)
 
-	conn, resp, err := client.ConnectionsApi.GetConnectionByUuid(ctx, "<connection_uuid>").Execute()
+	// From here to the end of the example we are defining the details of our GET /fabric/v4/connections/{connectionId}
+	// request that will retrieve a Fabric Connection from its UUID
+	conn, resp, err := client.ConnectionsApi.GetConnectionByUuid(ctx, connection_uuid).Execute()
 
 	if err != nil {
 		log.Printf("Error getting Fabric Connection: %v", err)
